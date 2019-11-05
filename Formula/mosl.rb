9a685c63eadb5eae53f7cbaa9c1a0205ba81b9b8
0xmachos <0xmachos@gmail.com>
2019-11-05 00:48:41 +0000

    desc: Update to Catalina (10.15.x)

--- Formula/mosl.rb
+++ Formula/mosl.rb
@@ -1,5 +1,5 @@
 class Mosl < Formula
-  desc "Bash script to audit and fix macOS Mojave (10.14.x) security settings"
+  desc "Bash script to audit and fix macOS Catalina (10.15.x) security settings"
   homepage "https://github.com/0xmachos/mOSL"
   url "https://github.com/0xmachos/mOSL/archive/v3.0.0.tar.gz"
   sha256 "77554c269147ef7677d5e4e5cecb3ca06fd53df9743e52a0b4d00d3f69670c1a"

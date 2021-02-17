59ecbaaba50cbb0c6519c77bc5414d1ba483320a
0xmachos <0xmachos@gmail.com>
2021-02-17 13:56:51 +0000

    url, sha256: Version bump to v3.2.1

--- Formula/mosl.rb
+++ Formula/mosl.rb
@@ -1,8 +1,8 @@
 class Mosl < Formula
   desc "Bash script to audit and fix macOS Catalina (10.15.x) security settings"
   homepage "https://github.com/0xmachos/mOSL"
-  url "https://github.com/0xmachos/mOSL/archive/v3.0.0.tar.gz"
-  sha256 "77554c269147ef7677d5e4e5cecb3ca06fd53df9743e52a0b4d00d3f69670c1a"
+  url "https://github.com/0xmachos/mOSL/archive/v3.2.1.tar.gz"
+  sha256 "a1662d0daba2c074fb917aee919bf6adf7bc7bfa33a6188013fc6fb6305cfa1e"
 
   depends_on "minisign"
 
